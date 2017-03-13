362502e47830e343aade566409aff0c7984ec195
tdahlen@redhat.com <tdahlen@redhat.com>
2017-03-13 07:52:00 +0000

    update file /www/js/hello.js

--- www/js/hello.js
+++ www/js/hello.js
@@ -8,7 +8,7 @@ document.getElementById('say_hello').onclick = function () {
         }
       },
       function (res) {
-        document.getElementById('cloudResponse').innerHTML = "<p>" + res[0].productname + "</p>";
+        document.getElementById('cloudResponse').innerHTML = "<p>" + res.productname + "</p>";
       },
       function (code, errorprops, params) {
         alert('An error occured: ' + code + ' : ' + errorprops);
